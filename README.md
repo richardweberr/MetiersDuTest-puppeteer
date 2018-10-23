f474af151e69644ac0449af9ab1e7184128fc3ef
Philippe Le Van <philippe.levan@kibatic.com>
2018-10-23 10:13:46 +0200

    tests puppeteer, init

---
+++ README.md
@@ -0,0 +1,21 @@
+Tests avec puppeteer
+====================
+
+Quick start
+-----------
+
+```bash
+# installer puppeteer à partir du packagge.json
+npm install
+
+# lancer les tests
+npm test
+```
+
+Ce qu'il faut regarder
+----------------------
+
+Les premiers fichiers à regarder sont dans l'ordre :
+
+* tests/basic.test.js
+* tests/shorten.test.js
---
+++ tests/basic.test.js
@@ -0,0 +1,52 @@
+const timeout = 10000
+
+// série de tests sur la page d'accueil
+describe("Tests basiques", () => {
+    let page
+
+    // vérification du chargement de la page d'accueil
+    test('home', async () => {
+        // charger la page d'accueil
+        await page.goto('http://polr.campus-grenoble.fr')
+        // attendre que l'élément <body> soit chargé
+        await page.waitForSelector('body')
+        // récupérer le contenu de l'élément <body>
+        const html = await page.$eval('body', e => e.innerHTML)
+        // vérifier que dans cet élément Body on trouve "Polr du campus"
+        expect(html).toContain("Polr du campus")
+    }, timeout)
+
+    // parcours client avec about
+    test('home and about', async () => {
+        await page.goto('http://polr.campus-grenoble.fr')
+        await page.waitForSelector('#navbar li a')
+        // click sur le lien "About" de la navigation
+        await page.evaluate( () =>
+        {
+            Array
+                .from( document.querySelectorAll( '#navbar li a' ) )
+                .filter( el => el.textContent === 'About' )[0].click();
+        });
+        // on attent que l'élément ".about-contents" soit chargé
+        await page.waitForSelector('.about-contents')
+        // on récupère le code HTML
+        const html = await page.$eval('.about-contents', e => e.innerHTML)
+        // on vérifie qu'il contient la bonne chaîne de caractères
+        expect(html).toContain("powered by Polr 2")
+    }, timeout)
+
+
+    // cette fonction est lancée avant chaque test de cette
+    // série de tests
+    beforeAll(async () => {
+        // ouvrir un onglet dans le navigateur
+        page = await global.__BROWSER__.newPage()
+    }, timeout)
+
+    // cette fonction est lancée après chaque test de cette
+    // série
+    afterAll(async () => {
+        // on referme l'onglet du navigateur
+        page.close()
+    }, timeout)
+})
