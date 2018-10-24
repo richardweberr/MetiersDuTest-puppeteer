e313f8d114c924fae1ef1352aa50410b53f4e036
Philippe Le Van <philippe.levan@kibatic.com>
2018-10-24 14:23:25 +0200

    wait for shorten

--- tests/shorten-anonymous.test.js
+++ tests/shorten-anonymous.test.js
@@ -10,6 +10,7 @@ describe("Shorten Anonymous", () => {
         await page.waitForSelector('.long-link-input')
         await page.type('.long-link-input', 'https://www.google.com/search?source=hp&ei=QQbPW52GC9CRlwSHw46oAg&q=puppeteer+jest&oq=puppeteer+jest&gs_l=psy-ab.3...2441.6095.0.6926.0.0.0.0.0.0.0.0..0.0....0...1c.1.64.psy-ab..0.0.0....0.qKd5wLlrTYk');
         await page.screenshot({path: './tests/img/shorten1.png'});
+        await page.waitForSelector('#shorten')
         await page.$eval( '#shorten', el => el.click() );
         await page.waitForSelector('#short_url')
         const val = await page.$eval('#short_url', el => el.value)
